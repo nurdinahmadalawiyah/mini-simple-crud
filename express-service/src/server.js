c0613cfacdb87c8454c592cdddf8b65635016b81
Nurdin Alawiyah <nurdinalawiyah@192.168.1.5>
2024-04-05 10:54:29 +0700

    add cors

--- express-service/src/server.js
+++ express-service/src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import cors from "cors";
 import customerRoutes from "./routes/customerRoute.js";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 app.use('/api', customerRoutes);
 
 app.listen(3000, () => {
